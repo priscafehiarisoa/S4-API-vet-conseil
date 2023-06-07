bfa3cb606e8e01340e3e517f439405b12e6e45b7
priscafehiarisoa <mytyrealy@gmail.com>
2023-06-07 06:42:46 +0300

    refactor : creating app hebergement and adding the models related to the app test : add views test

---
+++ hebergement/models/animal_accepte.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from hebergement.models import Race
+
+
+class Animal_accepte(models.Model):
+    race=models.ForeignKey(Race,on_delete=models.CASCADE)
+    debut_interval_age=models.IntegerField()
+    fin_interval_age=models.IntegerField()
+    debut_interval_validite=models.DateField()
+    fin_interval_validite=models.DateField()
--- vet_conseil/settings.py
+++ vet_conseil/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #
+    'hebergement.apps.HebergementConfig',
+    # ----
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
