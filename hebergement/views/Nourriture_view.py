4da7a54af0de8c46ee16b006922ce300f486354d
anthony <anthonyarimalala@gmail.com>
2023-06-26 10:31:21 +0300

    feat

--- hebergement/views/Nourriture_view.py
+++ hebergement/views/Nourriture_view.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from hebergement.models import  Attribution
 from hebergement.models import Race
+from hebergement.models import Nourriture
 
 def load_list_animals_food(request):
     id_race = request.GET.get('id_race')
@@ -23,4 +24,27 @@ def load_list_animals_food(request):
 
 
 def add_new_type_food(request):
-    return render(request,"hebergement/nourriture/ajouter_nourriture.html")
+    description = request.GET.get('description')
+    designation = request.GET.get('designation')
+    prix_par_unite = request.GET.get('prix_unite')
+    unite = request.GET.get('unite')
+    if(description != None):
+        new_food = Nourriture(designation=designation, description=description, prix_par_unite=prix_par_unite, unite=unite)
+        new_food.save()
+        new_food_id = new_food.id
+
+        if request.method == 'GET':
+            races_selectionnees = request.GET.getlist('attribution_checkbox')
+            debut_interval_poids = 0
+            fin_interval_poids = 100
+
+            nourriture = Nourriture.objects.get(id=new_food_id)
+
+            for race_id in races_selectionnees:
+                race = Race.objects.get(id=race_id)
+                attribution = Attribution(nourriture=nourriture, race=race, debut_interval_poids=debut_interval_poids, fin_interval_poids=fin_interval_poids)
+                attribution.save()
+
+
+    races = Race.objects.all()
+    return render(request,"hebergement/nourriture/ajouter_nourriture.html", {"races": races})
